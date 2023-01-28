59270c7335c1ee139fee79544a9df1d6855566ee
SwamyK22 <swamy199722@gmail.com>
2023-01-28 11:01:36 +0530

    first_setup_React_code

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path=require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use:"babel-loader"
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template:"./public/index.html",
+        filename:"index.html"
+    })],
+    mode:"development",
+    devServer:{
+        port:"3004",
+        open:true,
+    }
+  };
